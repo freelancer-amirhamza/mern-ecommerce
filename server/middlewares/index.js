68f4dfe4384c8542739444edcb1a07716bfa7bb7
Amirhamza <amirhamza27940@gmail.com>
2025-01-20 10:35:26 +0600

    some change

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,23 +1,25 @@
 const jwt = require("jsonwebtoken");
 
 
-const authMiddleware =  (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token)
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]
+  if (!token)
       return res.status(401).json({
-        success: false,
-        message: "Unauthorized user!",
-    });
-    try {
+          success: false,
+          message: "Unauthorized user!",
+      });
+  try {
       const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
       req.user = decoded;
       next();
-    } catch (error) {
+  } catch (error) {
       res.status(401).json({
-        success: false,
-        message: "Unauthorized user!",
+          success: false,
+          message: "Unauthorized user!",
       });
-    }
-  };
+  }
+};
+
 
 module.exports = authMiddleware;
